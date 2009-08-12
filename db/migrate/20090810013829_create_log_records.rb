c1ffa7805ae21f6ce17b48393aeddc30bdf9690d
Alexey Kovyrin <alexey@kovyrin.net>
2009-08-12 02:44:01 -0400

    Simplify migration. Add specs for DbCharmer exception control attributes

--- db/migrate/20090810013829_create_log_records.rb
+++ db/migrate/20090810013829_create_log_records.rb
@@ -1,17 +1,15 @@
 class CreateLogRecords < ActiveRecord::Migration
+  db_magic :connection => :logs
+  
   def self.up
-    on_db :logs do
-      create_table :log_records do |t|
-        t.string :level
-        t.string :message
-        t.timestamps
-      end
+    create_table :log_records do |t|
+      t.string :level
+      t.string :message
+      t.timestamps
     end
   end
 
   def self.down
-    on_db :logs do
-      drop_table :log_records
-    end
+    drop_table :log_records
   end
 end
