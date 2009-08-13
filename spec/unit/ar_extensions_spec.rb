db152e9b10e2ac6a46664b1db393d9539b83e1da
Alexey Kovyrin <alexey@kovyrin.net>
2009-08-13 12:15:03 -0400

    Connection levels tests added + random slaves selection tested

--- spec/unit/ar_extensions_spec.rb
+++ spec/unit/ar_extensions_spec.rb
@@ -45,7 +45,42 @@ describe DbCharmer, "for ActiveRecord models" do
       FooModel.db_charmer_slaves = [ proxy ]
       FooModel.db_charmer_slaves.should == [ proxy ]
     end
+    
+    it "should implement random slave selection" do
+      FooModel.db_charmer_slaves = [ :proxy1, :proxy2, :proxy3 ]
+      srand(0)
+      FooModel.db_charmer_random_slave.should == :proxy1
+      FooModel.db_charmer_random_slave.should == :proxy2
+      FooModel.db_charmer_random_slave.should == :proxy1
+      FooModel.db_charmer_random_slave.should == :proxy2
+      FooModel.db_charmer_random_slave.should == :proxy2
+      FooModel.db_charmer_random_slave.should == :proxy3
+    end
   end
+
+  describe "in db_charmer_connection_levels methods" do
+    it "should return 0 by default" do
+      FooModel.db_charmer_connection_level = nil
+      FooModel.db_charmer_connection_level.should == 0
+    end
+    
+    it "should implement both accessor methods and support inc/dec operations" do
+      FooModel.db_charmer_connection_level = 1
+      FooModel.db_charmer_connection_level.should == 1
+      FooModel.db_charmer_connection_level += 1
+      FooModel.db_charmer_connection_level.should == 2
+      FooModel.db_charmer_connection_level -= 1
+      FooModel.db_charmer_connection_level.should == 1
+    end
+    
+    it "should implement db_charmer_top_level_connection? method" do
+      FooModel.db_charmer_connection_level = 1
+      FooModel.should_not be_db_charmer_top_level_connection
+      FooModel.db_charmer_connection_level = 0
+      FooModel.should be_db_charmer_top_level_connection
+    end
+  end
+
   
   describe "in connection method" do
     it "should return AR's original connection if no connection proxy is set" do
