8b696147b0272cfc19f53b55fa0b175f17ea4c12
Alexey Kovyrin <alexey@kovyrin.net>
2009-08-13 15:49:14 -0400

    More comprehensive calculation tests

--- spec/unit/finder_overrides_spec.rb
+++ spec/unit/finder_overrides_spec.rb
@@ -37,20 +37,20 @@ describe "ActiveRecord slave-enabled models" do
       describe meth do
         it "should go to the slave if called on the first level connection" do
           User.on_slave.connection.should_receive(:select_value).and_return(1)
-          User.send(meth, :id)
+          User.send(meth, :id).should == 1
         end
   
         it "should not change connection if called in an on_db block" do
           User.on_db(:logs).connection.should_receive(:select_value).and_return(1)
           User.on_slave.connection.should_not_receive(:select_value)
-          User.on_db(:logs).send(meth, :id)
+          User.on_db(:logs).send(meth, :id).should == 1
         end
 
         it "should not change connection when it's already been changed by on_slave call" do
           User.on_slave do
             User.on_slave.connection.should_receive(:select_value).and_return(1)
             User.should_not_receive(:on_db)
-            User.send(meth, :id)
+            User.send(meth, :id).should == 1
           end
         end
       end
