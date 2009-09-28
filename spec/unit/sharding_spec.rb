70b2b0c6be6fcf24a06f12be0bd621b3814a4211
Alexey Kovyrin <alexey@kovyrin.net>
2009-09-28 13:34:48 -0400

    Added a few more tests for sharding code

--- spec/unit/sharding_spec.rb
+++ spec/unit/sharding_spec.rb
@@ -1,2 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+describe "DbCharmer::Sharding" do
+  describe "in register_connection method" do
+    it "should raise an exception if passed config has no :name option" do
+      lambda {
+        DbCharmer::Sharding.register_connection(:method => :range, :ranges => { :default => :foo })
+      }.should raise_error(ArgumentError)
+    end
+  end
+
+  describe "in sharded_connection method" do
+    it "should raise an error for invalid connection names" do
+      lambda { DbCharmer::Sharding.sharded_connection(:blah) }.should raise_error(ArgumentError)
+    end
+  end
+end
