37a2d649d4c3dac134e2bd092cb1cd899510f0fe
Alexey Kovyrin <alexey@kovyrin.net>
2009-09-30 16:34:39 -0400

    Fix scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   db_magic :slave => :slave01
   belongs_to :user
 
-  named_scope :windows_posts, "title like '%win%'"
+  named_scope :windows_posts, :conditions => "title like '%win%'"
 end
