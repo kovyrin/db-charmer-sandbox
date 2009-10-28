b0b57e9c9b662ef47a31f0c96bc55ae2007f7fc4
Alexey Kovyrin <alexey@kovyrin.net>
2009-10-28 00:01:26 -0400

    Added named scopes support: Model.on_db(:foo).scope.action and Model.scope.on_db(:foo).action are supported

--- spec/unit/named_scope_spec.rb
+++ spec/unit/named_scope_spec.rb
@@ -2,7 +2,28 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Named scopes" do
   fixtures :users, :posts
-  it "should work with on_db proxy" do
-    Post.on_db(:slave_db01).windows_posts.should == Post.windows_posts
+
+  describe "prefixed by on_db" do
+    it "should work on the proxy" do
+      Post.on_db(:slave_db01).windows_posts.should == Post.windows_posts
+    end
+
+    it "should actually run queries on the specified db" do
+      Post.on_db(:slave_db01).connection.should_receive(:select_all).once.and_return([])
+      Post.on_db(:slave_db01).windows_posts.all
+      Post.windows_posts.all
+    end
+  end
+
+  describe "postfixed by on_db" do
+    it "should work on the proxy" do
+      Post.windows_posts.on_db(:slave_db01).should == Post.windows_posts
+    end
+
+    it "should actually run queries on the specified db" do
+      Post.on_db(:slave_db01).connection.should_receive(:select_all).once.and_return([])
+      Post.windows_posts.on_db(:slave_db01).all
+      Post.windows_posts.all
+    end
   end
 end
