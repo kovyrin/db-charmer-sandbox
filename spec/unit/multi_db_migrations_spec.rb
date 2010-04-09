70dfc12644af087603702974aee29c7ecb252ef6
Alexey Kovyrin <alexey@kovyrin.net>
2010-04-09 13:19:26 -0400

    Default migration connection specification is possible now

--- spec/unit/multi_db_migrations_spec.rb
+++ spec/unit/multi_db_migrations_spec.rb
@@ -1,5 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+class SpecMigration < ActiveRecord::Migration
+  def self.up
+    execute "UPDATE log_records SET level = 'debug'"
+  end
+
+  def self.down
+    execute "UPDATE log_records SET level = 'blah'"
+  end
+end
+
 class SpecMultiDbMigration < ActiveRecord::Migration
   db_magic :connection => :logs
 
@@ -57,6 +67,28 @@ describe "Multi-db migractions" do
     DbCharmer.connections_should_exist = false
   end
 
+  describe "w/o any magic calls" do
+    it "should send all up requests to the default connection" do
+      ActiveRecord::Base.connection.should_receive(:execute).with("UPDATE log_records SET level = 'debug'")
+      SpecMigration.migrate(:up)
+    end
+
+    it "should send all down requests to the default connection" do
+      ActiveRecord::Base.connection.should_receive(:execute).with("UPDATE log_records SET level = 'blah'")
+      SpecMigration.migrate(:down)
+    end
+
+    describe "after AR::Migration db_magic call" do
+      it "should use default migration config" do
+        ActiveRecord::Migration.db_magic :connection => :logs
+        ActiveRecord::Base.connection.should_not_receive(:execute)
+        DbCharmer::ConnectionFactory.connect(:logs).should_receive(:execute).with("UPDATE log_records SET level = 'debug'")
+        SpecMigration.migrate(:up)
+        ActiveRecord::Migration.db_magic :connection => :default
+      end
+    end
+  end
+
   describe "with db_magic calls" do
     it "should send all up requests to specified connection" do
       ActiveRecord::Base.connection.should_not_receive(:execute)
@@ -68,6 +100,17 @@ describe "Multi-db migractions" do
       ActiveRecord::Base.connection.should_not_receive(:execute)
       DbCharmer::ConnectionFactory.connect(:logs).should_receive(:execute).with("UPDATE log_records SET level = 'blah'")
       SpecMultiDbMigration.migrate(:down)
+    end
+
+    describe "after AR::Migration db_magic call" do
+      it "should use spcified connection and ignore global migration config" do
+        ActiveRecord::Migration.db_magic :connection => :slave01
+        ActiveRecord::Base.connection.should_not_receive(:execute)
+        DbCharmer::ConnectionFactory.connect(:slave01).should_not_receive(:execute)
+        DbCharmer::ConnectionFactory.connect(:logs).should_receive(:execute).with("UPDATE log_records SET level = 'debug'")
+        SpecMultiDbMigration.migrate(:up)
+        ActiveRecord::Migration.db_magic :connection => :default
+      end
     end
   end
 
