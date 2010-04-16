30dedacd3d38b7382d8c16e0e3711b732c0d8503
Alexey Kovyrin <alexey@kovyrin.net>
2010-04-16 00:09:27 -0400

    We require connections to exist by default now

--- db/create_databases.sql
+++ db/create_databases.sql
@@ -1,11 +1,9 @@
-# Test databases
+drop database db_charmer_sandbox_test;
 create database if not exists db_charmer_sandbox_test;
+drop database db_charmer_logs_test;
 create database if not exists db_charmer_logs_test;
-
-# Events shards for testing
+drop database db_charmer_events_test_shard01;
 create database if not exists db_charmer_events_test_shard01;
+drop database db_charmer_events_test_shard02;
 create database if not exists db_charmer_events_test_shard02;
-
-# Grant privileges
-grant all privieges on db_charmer_sandbox_test.* to db_charmer_ro@'localhost';
-
+#grant all privieges on db_charmer_sandbox_test.* to 'db_charmer_ro'@'localhost';
