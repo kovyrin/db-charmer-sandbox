4e2e2314ce954719aedef5e0f937a4449998d5e0
Oleksiy Kovyrin <alexey@kovyrin.net>
2011-06-29 14:18:42 -0400

    Create databases on ci

--- db/create_databases.sql
+++ db/create_databases.sql
@@ -1,9 +1,13 @@
-drop database db_charmer_sandbox_test;
-create database if not exists db_charmer_sandbox_test;
-drop database db_charmer_logs_test;
-create database if not exists db_charmer_logs_test;
-drop database db_charmer_events_test_shard01;
-create database if not exists db_charmer_events_test_shard01;
-drop database db_charmer_events_test_shard02;
-create database if not exists db_charmer_events_test_shard02;
-#grant all privieges on db_charmer_sandbox_test.* to 'db_charmer_ro'@'localhost';
+drop database if exists db_charmer_sandbox_test;
+create database db_charmer_sandbox_test;
+
+drop database if exists db_charmer_logs_test;
+create database db_charmer_logs_test;
+
+drop database if exists db_charmer_events_test_shard01;
+create database db_charmer_events_test_shard01;
+
+drop database if exists db_charmer_events_test_shard02;
+create database db_charmer_events_test_shard02;
+
+grant all privieges on db_charmer_sandbox_test.* to 'db_charmer_ro'@'localhost';
