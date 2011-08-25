bee8f6c160949f415929b45ebe78595a31bc2667
Oleksiy Kovyrin <alexey@kovyrin.net>
2011-08-25 16:17:16 -0400

    Added back specs for the force slave reads functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ DbCharmerSandbox::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  # Resource routes
+  resources :posts
+  resources :cars
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -54,5 +58,5 @@ DbCharmerSandbox::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
