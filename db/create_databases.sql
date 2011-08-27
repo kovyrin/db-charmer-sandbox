9c0970ffeff96f4adc0c24e43331ee5ad4c4bfc0
Oleksiy Kovyrin <alexey@kovyrin.net>
2011-08-27 01:41:48 -0400

    Don't need this db names mess anymore

--- db/create_databases.sql
+++ db/create_databases.sql
@@ -1,13 +1,13 @@
-drop database if exists db_charmer_sandbox22_test;
-create database db_charmer_sandbox22_test;
+drop database if exists db_charmer_sandbox_test;
+create database db_charmer_sandbox_test;
 
-drop database if exists db_charmer_logs22_test;
-create database db_charmer_logs22_test;
+drop database if exists db_charmer_logs_test;
+create database db_charmer_logs_test;
 
-drop database if exists db_charmer_events22_test_shard01;
-create database db_charmer_events22_test_shard01;
+drop database if exists db_charmer_events_test_shard01;
+create database db_charmer_events_test_shard01;
 
-drop database if exists db_charmer_events22_test_shard02;
-create database db_charmer_events22_test_shard02;
+drop database if exists db_charmer_events_test_shard02;
+create database db_charmer_events_test_shard02;
 
-grant all privileges on db_charmer_sandbox22_test.* to 'db_charmer_ro'@'localhost';
+grant all privileges on db_charmer_sandbox_test.* to 'db_charmer_ro'@'localhost';
